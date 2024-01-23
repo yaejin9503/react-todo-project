930d16a6387f9cb7c248cfa4ecf4fee4cfb4c2a6
yejin <jimmy0152@naver.com>
2024-01-23 22:18:55 +0900

    Add : 기분 기능 추가를 위한 마크업

--- src/Header/TodoHeader.tsx
+++ src/Header/TodoHeader.tsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import { useTodoState } from '../Todo/todoProvider';
-import { TodoType } from '../Todo/todoReducer';
 import styles from './TodoHeader.module.css' 
-
-// interface TodoHeaderProps { 
-//   count: number
-// }
-
 
 const TodoHeader = () => {  
   const todoState =  useTodoState(); 
+  const [ isOpen, setIsOpen ] = useState(false); 
   const count = todoState.todos.filter(todo => !todo.isCheck).length
   return( 
     <header> 
+      <div className={styles.imojiWrap}>
+        <button className={styles.imojiBtn} onClick={() => setIsOpen(!isOpen)}>
+          <span className={styles.imoji}>😄</span>
+        </button>
+          <div className={isOpen ? [ styles.imojiTooltip , styles.show].join(' ') : styles.imojiTooltip}>
+            <div className={styles.imojiSelect}>
+              <span>🥰</span>
+            </div>
+            <div className={styles.imojiSelect}>
+              <span>😡</span>
+            </div>
+            <div className={styles.imojiSelect}>
+              <span>😰</span>
+            </div>
+            <div>
+              <span>🥲</span>
+            </div>
+          </div>
+      </div>
       <h1 className={styles.headerTitle}>
         <mark className={styles.todoCount}>{count}</mark>개의 할일
       </h1>
